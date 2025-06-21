9d48a4edc7d82bb13c62d702c223be40f718a5f3
Md Sowrov <antukhadem17@gmail.com>
2025-06-21 12:47:44 +0600

    Created chat page

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -3,8 +3,8 @@ import NavItem from "./NavItem";
 
 const Sidebar = () => {
   return (
-    <aside className="h-screen  hidden sm:block w-[250px] lg:w-[280px] ">
-      <div className="text-center py-3 border-b border-b-gray-700">
+    <aside className="h-screen  hidden sm:block w-[250px] lg:w-[280px] border-r border-r-gray-700/30 ">
+      <div className="text-center py-3 border-b border-b-gray-700/30">
         <Logo />
       </div>
       <div className="mt-5">
